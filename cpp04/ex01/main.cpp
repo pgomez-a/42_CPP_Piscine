dc75008e4dbd0cce4478ded2ee71d9d79d7bddec
pgomez-a <pablogomezalvarez02@gmail.com>
2021-10-17 22:53:36 +0200

    Update cpp04 ex01 - Add search_leaks - Update changes from ex00

--- cpp04/ex01/main.cpp
+++ cpp04/ex01/main.cpp
@@ -4,39 +4,46 @@
 #include "Cat.hpp"
 #include <iostream>
 
+static void	search_leaks(void)
+{
+	system("leaks call_animals");
+}
+
 int	main(void)
 {
-	const Animal*	j = new Dog();
-	const Animal*	i = new Cat();
-	const Animal*	animal[4] = {new Dog(*j), new Dog(*j), new Cat(*i), new Cat(*i)};
-	int		count;
-
-	std::cout << std::endl << std::endl;
-
-	std::cout << "j ------> " << j << " -> " << j->getBrain() << std::endl;
-	std::cout << "i ------> " << i << " -> " << i->getBrain() << std::endl;
-	count = 0;
-	while (count < 4)
+	atexit(search_leaks);
 	{
-		std::cout << "animal[" << count << "] " << animal[count]
-			<< " -> " << animal[count]->getType()
-			<< " -> " << animal[count]->getBrain()
-			<< std::endl;
-		count++;
+		const Animal*	j = new Dog();
+		const Animal*	i = new Cat();
+		const Animal*	animal[4] = {new Dog(*j), new Dog(*j), new Cat(*i), new Cat(*i)};
+		int		count;
+
+		std::cout << std::endl << std::endl;
+		std::cout << "j ------> " << j << " -> " << j->getBrain() << std::endl;
+		std::cout << "i ------> " << i << " -> " << i->getBrain() << std::endl;
+		count = 0;
+		while (count < 4)
+		{
+			std::cout << "animal[" << count << "] " << animal[count]
+				<< " -> " << animal[count]->getType()
+				<< " -> " << animal[count]->getBrain()
+				<< std::endl;
+			count++;
+		}
+
+		std::cout << std::endl << std::endl;
+		count = 0;
+		while (count < 4)
+		{
+			delete(animal[count]);
+			count++;
+		}
+
+		std::cout << std::endl << std::endl;
+		delete j;
+		delete i;
 	}
-
-	std::cout << std::endl << std::endl;
-
-	count = 0;
-	while (count < 4)
-	{
-		delete(animal[count]);
-		count++;
-	}
-
-	std::cout << std::endl << std::endl;
-
-	delete j;
-	delete i;
+	std::cout << std::endl;
+	std::cout << std::endl;
 	return (0);
 }
