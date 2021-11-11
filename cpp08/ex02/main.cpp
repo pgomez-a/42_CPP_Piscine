ed817f28ec4bcfbf3cc2c973fae6ba21c88f1e96
pgomez-a <pablogomezalvarez02@gmail.com>
2021-11-11 23:01:00 +0100

    Update cpp08 ex02 - Change everything - Now MutantStack inherit from std::stack

--- cpp08/ex02/main.cpp
+++ cpp08/ex02/main.cpp
@@ -2,25 +2,25 @@
 
 int main()
 {
-	MutantStack<int> mstack;
+	MutantStack<int>	mstack;
 
 	mstack.push(5);
 	mstack.push(17);
 
-	std::cout << "top: " << mstack.top() << std::endl;
+	std::cout << mstack.top() << std::endl;
 
 	mstack.pop();
 
-	std::cout << "size: " << mstack.size() << std::endl;
+	std::cout << mstack.size() << std::endl;
 
 	mstack.push(3);
 	mstack.push(5);
 	mstack.push(737);
 
-	MutantStack<int>::iterator it = mstack.begin();
-	MutantStack<int>::iterator ite = mstack.end();
+	mstack.push(0);
 
-	std::cout << std::endl;
+	MutantStack<int>::iterator	it = mstack.begin();
+	MutantStack<int>::iterator	ite = mstack.end();
 
 	++it;
 	--it;
@@ -29,6 +29,7 @@ int main()
 		std::cout << *it << std::endl;
 		++it;
 	}
-	//std::stack<int> s(mstack);
+	std::stack<int> s(mstack);
+
 	return 0;
 }
--- cpp08/ex02/mutantstack.hpp
+++ cpp08/ex02/mutantstack.hpp
@@ -2,215 +2,41 @@
 # define MUTANTSTACK_HPP
 
 # include <iostream>
-# include <list>
+# include <stack>
+# include <deque>
 
-template < typename T >
-class	MutantStack
+template < typename T, typename Container = std::deque<T> >	
+class	MutantStack : public std::stack<T, Container>
 {
 	public:
-		MutantStack(void)
-		{
-			return ;
-		}
+		MutantStack(void) : std::stack<T, Container>() {}
+		MutantStack(MutantStack const & mstack) : std::stack<T, Container>(mstack) {}
+		MutantStack(std::stack<T> const & stack) : std::stack<T, Container>(stack) {}
+		virtual ~MutantStack(void) {}
 
-		MutantStack(MutantStack const & mstack)
-		{
-			*this = mstack;
-			return ;
-		}
 
-		~MutantStack(void)
-		{
-			return ;
-		}
-
-		MutantStack&	operator=(MutantStack const & mstack)
-		{
-			MutantStack*			ptr_mstack;
-			typename std::list<T>::iterator	beg;
-
-			if (this != &mstack)
-			{
-				this->_data.clear();
-				ptr_mstack = const_cast<MutantStack *>(&mstack);
-				beg = ptr_mstack->_data.begin();
-				while (beg !=  ptr_mstack->_data.end())
-				{
-					this->_data.push_back(*beg);
-					beg++;
-				}
-			}
-			return (*this);
-		}
-
-		bool		empty(void) const
-		{
-			return (this->_data.empty());
-		}
-
-		size_t		size(void) const
-		{
-			return (this->_data.size());
-		}
-
-		T&		top(void)
-		{
-			return (*(this->_data.begin()));
-		}
-
-		T const &	top(void) const
-		{
-			return (*(this->_data.begin()));
-		}
-
-		void		push(T const & val)
-		{
-			T	elem;
-
-			elem = val;
-			this->_data.push_front(elem);
-			return ;
-		}
-
-		void		pop(void)
-		{
-			this->_data.pop_front();
-			return ;
-		}
-
-		class	iterator
-		{
-			public:
-				iterator(void)
-				{
-					return ;
-				}
-
-				iterator(iterator const & it)
-				{
-					this->_pointer = it._pointer;
-					return ;
-				}
-
-				~iterator(void)
-				{
-					return ;
-				}
-
-				iterator&	operator=(iterator const & it)
-				{
-					this->_pointer = it._pointer;
-					return (*this);
-				}
-
-				iterator&	operator=(typename std::list<T>::iterator const it)
-				{
-					this->_pointer = it; 
-					return (*this);
-				}
-
-				T		operator*(void) const
-				{
-					return (*(this->_pointer));
-				}
-
-				iterator	operator++(int)
-				{
-					iterator	output;
-
-					output = *this;
-					this->_pointer++;
-					return (output);
-				}
-
-				iterator	operator++(void)
-				{
-					iterator	output;
-
-					this->_pointer++;
-					output = *this;
-					return (output);
-				}
-
-				iterator	operator--(int)
-				{
-					iterator	output;
-
-					output = *this;
-					this->_pointer--;
-					return (output);
-				}
-
-				iterator	operator--(void)
-				{
-					iterator	output;
-
-					this->_pointer--;
-					output = *this;
-					return (output);
-				}
-
-				bool				operator==(iterator const & it)
-				{
-					if (this->_pointer == it._pointer)
-						return (true);
-					return (false);
-				}
-
-				bool				operator!=(iterator const & it)
-				{
-					if (this->_pointer != it._pointer)
-						return (true);
-					return (false);
-				}
-
-				bool				operator==(typename std::list<T>::iterator it)
-				{
-					if (this->_pointer == it)
-						return (true);
-					return (false);
-				}
-
-				bool				operator!=(typename std::list<T>::iterator it)
-				{
-					if (this->_pointer != it)
-						return (true);
-					return (false);
-				}
-
-				void				setPointer(typename std::list<T>::iterator it)
-				{
-					this->_pointer = it;
-					return ;
-				}
-
-				typename std::list<T>::iterator	getPointer(void) const
-				{
-					return (this->_pointer);
-				}
-
-			private:
-				typename std::list<T>::iterator	_pointer;
-		};
+		typedef typename std::stack<T, Container>::container_type::iterator iterator;
 
 		iterator	begin(void)
 		{
-			iterator	output;
+			return (std::begin(std::stack<T, Container>::c));
+		}
 
-			output.setPointer(this->_data.begin());
-			return (output);
+		iterator const	begin(void) const
+		{
+			return (std::begin(std::stack<T, Container>::c));
 		}
 
 		iterator	end(void)
 		{
-			iterator	output;
-
-			output.setPointer(this->_data.end());
-			return (output);
+			return (std::end(std::stack<T, Container>::c));
 		}
 
-	private:
-		std::list<T>	_data;
+		iterator const	end(void) const
+		{
+			return (std::end(std::stack<T, Container>::c));
+		}
+
 };
 
 #endif
